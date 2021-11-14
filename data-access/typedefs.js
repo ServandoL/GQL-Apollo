7092f2089532db0ca93c50fb5eab847a2b235532
Servando <swyswy214@gmail.com>
2021-11-14 11:04:42 -0600

    update apollo server

---
+++ data-access/typedefs.js
@@ -0,0 +1,22 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    sessions: [Session]
+  }
+
+  type Session {
+    id: ID!
+    title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String
+    level: String
+  }
+`;
+
+module.exports = typeDefs;
